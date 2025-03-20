d69fac419abd40d6d7404502a7a4cd6bbb075c92
이예은 한화시스템/ICT <lena0323park@naver.com>
2025-03-20 14:06:43 +0900

    250320 14:06

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,28 +1,60 @@
 // import { useState} from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import "./App.css";
+import { useReducer } from "react";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
+import Header from "./components/Header";
+import Edit from "./pages/Edit";
+import { getEmotionImage } from "./util/get-emotion-image";
+import Button from "./components/Button";
 
+const mockData = [
+  {
+    id: 1,
+    createdAt: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기",
+  },
+  {
+    id: 2,
+    createdAt: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기",
+  },
+  {
+    id: 3,
+    createdAt: new Date().getTime(),
+    emotionId: 3,
+    content: "3번 일기",
+  },
+];
+function reducer(state, action) {
+  return state;
+}
 function App() {
-  const nav = useNavigate();
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [data, dispatch] = useReducer(reducer, mockData);
   return (
     <>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-        <button onClick={onClickButton}>New 페이지로 이동</button>
-      </div>
+      {/* <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+      <Button
+        text={"123"}
+        type={"DEFAULT"}
+        onClick={() => {
+          console.log("object");
+        }}
+      /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
